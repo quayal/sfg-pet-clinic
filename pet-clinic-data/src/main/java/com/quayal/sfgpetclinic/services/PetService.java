153f1dee744fcfe68de05eedbbd4a763c7f0c5eb
quayal <adrian.grzebyk@gmail.com>
2018-12-26 11:54:56 +0100

    Base interface for services

--- pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/services/PetService.java
@@ -2,14 +2,5 @@ package com.quayal.sfgpetclinic.services;
 
 import com.quayal.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-	Pet findById(Long id);
-
-	Pet save(Pet pet);
-
-	Set<Pet> findAll();
-
+public interface PetService extends CrudService<Pet, Long> {
 }
