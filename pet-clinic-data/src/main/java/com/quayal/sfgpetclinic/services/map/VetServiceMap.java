9642e6bb010754ce4fd5c430fe107130e10fea15
quayal <adrian.grzebyk@gmail.com>
2019-01-13 16:53:16 +0100

    autonumbering for map service

--- pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/services/map/VetServiceMap.java
@@ -21,7 +21,7 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
 	@Override
 	public Vet save(Vet entity) {
-		return super.save(entity.getId(), entity);
+		return super.save(entity);
 	}
 
 	@Override
