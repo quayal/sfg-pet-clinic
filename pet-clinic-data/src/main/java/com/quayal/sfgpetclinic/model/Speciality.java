c8f622c8431d01ad8666b311c5795bc9a03d5fe7
quayal <adrian.grzebyk@gmail.com>
2019-02-24 20:47:24 +0100

    Create speciality entity; associate with vet. Closes #16

---
+++ pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.quayal.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	private String description;
+}
