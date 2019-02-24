c5f81ee2cb27900ea61233b300cf26db282b5ce4
quayal <adrian.grzebyk@gmail.com>
2019-02-24 20:41:46 +0100

    Added visit and updated owner; added i18n properties. closes #15

--- pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/model/Owner.java
@@ -1,4 +1,20 @@
 package com.quayal.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+	private Set<Pet> pets;
+
+	public Owner(Set<Pet> pets) {
+		this.pets = pets;
+	}
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 }
