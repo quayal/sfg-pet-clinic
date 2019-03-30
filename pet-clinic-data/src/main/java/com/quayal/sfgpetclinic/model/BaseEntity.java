74e5fafcabd63482ee2752a1af3a5bd8660f74bf
quayal <adrian.grzeby@gmail.com>
2019-03-30 11:18:12 +0100

    Base entity and oups

--- pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,18 @@
 package com.quayal.sfgpetclinic.model;
 
+import org.springframework.cglib.core.GeneratorStrategy;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
