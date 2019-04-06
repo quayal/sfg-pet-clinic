36075e56ce7f8b08e93799aa4ff887424f960aea
quayal <adrian.grzeby@gmail.com>
2019-04-06 13:25:27 +0200

    Owners to Entity closes #25

--- pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/model/Pet.java
@@ -1,12 +1,26 @@
 package com.quayal.sfgpetclinic.model;
 
+import lombok.extern.apachecommons.CommonsLog;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+	@Column(name = "name")
 	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "type_id")
 	private PetType petType;
+
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
 	private Owner owner;
+
+	@Column(name = "birth_date")
 	private LocalDate birthDate;
 
 	public String getName() {
