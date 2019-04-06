b085ff7659b7fd01c3f6a8d5deb9e748d301d41c
quayal <adrian.grzeby@gmail.com>
2019-04-06 15:35:22 +0200

    Visit Entity closes #27

--- pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/quayal/sfgpetclinic/model/Visit.java
@@ -1,11 +1,20 @@
 package com.quayal.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+	@Column(name = "date")
 	private LocalDate date;
+
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
+
+	@Column(name = "description")
 	private String description;
 
 	public LocalDate getDate() {
